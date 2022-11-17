83f09b77f127187a06e09f3e101518eb62554b38
Alex Smirnov <010407@gmail.com>
2022-11-17 18:40:36 +0300

    I am commiting file in another branch

--- main.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Hi there, general Kenobi!')
-    print('It works')
-
-
-if __name__ == '__main__':
-    main()
